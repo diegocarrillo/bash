874675454f9bae2ce12ad510d1facfc36969941d
diegocarrillo <diegocarrillo.cr@gmail.com>
2019-06-19 20:35:13 +0000

    Adding script to display env variables

---
+++ DisplayEnvVars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script will display some env variables
+
+echo "This is the current user: " $USER
+echo ""
+echo "This is the Home directory: " $HOME
+echo ""
+echo "This is the command history bash rule: " $HISTCONTROL
+echo ""
+echo "This is the terminal type: " $TERM
+
